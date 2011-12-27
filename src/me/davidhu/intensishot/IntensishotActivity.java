2f92e742193c19a1a89244cbcef5973ae10ec910
octopi <octopie@gmail.com>
2011-12-27 00:23:11 -0500

    countdown button

--- src/me/davidhu/intensishot/IntensishotActivity.java
+++ src/me/davidhu/intensishot/IntensishotActivity.java
@@ -6,15 +6,22 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class IntensishotActivity extends Activity implements SensorEventListener {
 	private SensorManager mSensorManager;
     private Sensor mAccelerometer;
     
+    private TextView countdownTimer;
+    
     private TextView xText;
     private TextView yText;
     private TextView zText;
+    
+    private Button button;
 	
     /** Called when the activity is first created. */
     @Override
@@ -22,18 +29,46 @@ public class IntensishotActivity extends Activity implements SensorEventListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        countdownTimer = (TextView) findViewById(R.id.countdownTimer);
+        
         xText = (TextView) findViewById(R.id.xText);
         yText = (TextView) findViewById(R.id.yText);
         zText = (TextView) findViewById(R.id.zText);
         
-        xText.setText("yo");
+        button = (Button) findViewById(R.id.button);
         
+        // start listening to sensor data at the start... is there a better way to do this?
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        
+        button.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				countdownTimer.setText("Clicked!");
+				CountDownTimer myTimer = new CountDownTimer(5000, 1000) {
+
+					@Override
+					public void onFinish() {
+						
+					}
+
+					@Override
+					public void onTick(long msLeft) {
+						countdownTimer.setText(String.valueOf((int)msLeft/1000));
+					}
+					
+				}.start();
+			}
+		});
+        
+        //xText.setText("yo");
+        
+        
     }
 
     protected void onResume() {
         super.onResume();
+        System.out.println("HERERERERE");
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
