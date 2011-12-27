ccc660a3810333b1579d1066f3194f791e07e542
octopi <octopie@gmail.com>
2011-12-27 14:45:10 -0500

    first run at graphics/visuals

--- gen/me/davidhu/intensishot/R.java
+++ gen/me/davidhu/intensishot/R.java
@@ -11,17 +11,23 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int backrepeat=0x7f020000;
+        public static final int brownbutton=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int logo_prod=0x7f020003;
+        public static final int wood_pattern=0x7f020004;
+        public static final int x9l0i24=0x7f020005;
     }
     public static final class id {
-        public static final int button=0x7f050004;
-        public static final int countdownTimer=0x7f050000;
-        public static final int xDiffText=0x7f050005;
-        public static final int xText=0x7f050001;
-        public static final int yDiffText=0x7f050006;
-        public static final int yText=0x7f050002;
-        public static final int zDiffText=0x7f050007;
-        public static final int zText=0x7f050003;
+        public static final int button=0x7f060008;
+        public static final int countdownTimer=0x7f060001;
+        public static final int imageView1=0x7f060000;
+        public static final int xDiffText=0x7f060005;
+        public static final int xText=0x7f060002;
+        public static final int yDiffText=0x7f060006;
+        public static final int yText=0x7f060003;
+        public static final int zDiffText=0x7f060007;
+        public static final int zText=0x7f060004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -30,5 +36,8 @@ public final class R {
         public static final int app_name=0x7f040001;
         public static final int button_label=0x7f040002;
         public static final int hello=0x7f040000;
+    }
+    public static final class style {
+        public static final int ButtonText=0x7f050000;
     }
 }
--- src/me/davidhu/intensishot/IntensishotActivity.java
+++ src/me/davidhu/intensishot/IntensishotActivity.java
@@ -8,6 +8,7 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -28,11 +29,15 @@ public class IntensishotActivity extends Activity implements SensorEventListener
 
 	private float[] zeroes; // original zeroed states of sensors
 	private float[] maxDiff; // max difference between zeroed and some observed state
+	
+	// this will continually be replaced as we log more "significant" pounds
+	private CountDownTimer endingTimer;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.main);
 
 		countdownTimer = (TextView) findViewById(R.id.countdownTimer);
@@ -49,22 +54,31 @@ public class IntensishotActivity extends Activity implements SensorEventListener
 		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
 		zeroes = null;
+		
+		endingTimer = null;
 
 		button.setOnClickListener(new View.OnClickListener() {
 
+			/**
+			 * After clicking, start countdown. 
+			 */
 			public void onClick(View v) {
 				countdownTimer.setText("Clicked!");
-				CountDownTimer myTimer = new CountDownTimer(5000, 1000) {
+				new CountDownTimer(6000, 1000) {
 
 					@Override
+					/**
+					 * After countdown, actually start the sensors. 
+					 */
 					public void onFinish() {
 						countdownTimer.setText("GO!");
 						startSensing();
+						restartEndTimer();
 					}
 
 					@Override
 					public void onTick(long msLeft) {
-						countdownTimer.setText(String.valueOf((int)msLeft/1000));
+						countdownTimer.setText(String.valueOf(((int)msLeft/1000)));
 					}
 
 				}.start();
@@ -74,6 +88,10 @@ public class IntensishotActivity extends Activity implements SensorEventListener
 
 	private void startSensing() {
 		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+	
+	private void stopSensing() {
+		mSensorManager.unregisterListener(this);
 	}
 
 	protected void onResume() {
@@ -112,18 +130,45 @@ public class IntensishotActivity extends Activity implements SensorEventListener
 
 			if(xDiff > maxDiff[0]) {
 				maxDiff[0] = xDiff;
+				restartEndTimer();
 			}
 			if(yDiff > maxDiff[1]) {
 				maxDiff[1] = yDiff;
+				restartEndTimer();
 			}
 			if(zDiff > maxDiff[2]) {
 				maxDiff[2] = zDiff;
+				restartEndTimer();
 			}
 
 			xDiffText.setText("x: "+String.valueOf(maxDiff[0]));
 			yDiffText.setText("y: "+String.valueOf(maxDiff[1]));
 			zDiffText.setText("z: "+String.valueOf(maxDiff[2]));    	
 		}
+	}
+	
+	/**
+	 * Restarts the timer that will end the sensing. 
+	 */
+	private void restartEndTimer() {
+		System.out.println("inside restart");
+		if(endingTimer != null) endingTimer.cancel();
+		endingTimer = new CountDownTimer(5000, 1000) {
+			
+			@Override
+			public void onTick(long millisUntilFinished) {
+				System.out.println("here");
+				countdownTimer.setText(String.valueOf(((int)millisUntilFinished/1000)));
+			}
+			
+			@Override
+			public void onFinish() {
+				stopSensing();
+				float score = maxDiff[0]+maxDiff[1]+maxDiff[2];
+				countdownTimer.setText("Done! Your score is: "+score);
+			}
+		}.start();
+		System.out.println("after timer");
 	}
 
 }
