ce713e04272fddcad532853268ba9a9ba83df33b
octopi <octopie@gmail.com>
2011-12-27 21:26:44 -0500

    new UX experiments

---
+++ src/me/davidhu/intensishot/IntensishotMainActivity.java
@@ -0,0 +1,13 @@
+package me.davidhu.intensishot;
+
+import android.app.Activity;
+
+/**
+ * Playing around with new UX
+ * 
+ * @author d
+ *
+ */
+public class IntensishotMainActivity extends Activity {
+	
+}
