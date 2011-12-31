d04793ea43323f4908e094a6bd270b7a2058c796
octopi <octopie@gmail.com>
2011-12-31 13:49:44 -0500

    cleaned up graphics in root

--- gen/me/davidhu/intensishot/R.java
+++ gen/me/davidhu/intensishot/R.java
@@ -14,47 +14,38 @@ public final class R {
         public static final int black=0x7f040000;
     }
     public static final class drawable {
-        public static final int backrepeat=0x7f020000;
-        public static final int brownbutton=0x7f020001;
-        public static final int dark_bg=0x7f020002;
-        public static final int graybutton=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int icon=0x7f020005;
-        public static final int logo2=0x7f020006;
-        public static final int logo_prod=0x7f020007;
-        public static final int meter=0x7f020008;
-        public static final int meter_dim=0x7f020009;
-        public static final int meter_dim0=0x7f02000a;
-        public static final int meter_dim1=0x7f02000b;
-        public static final int meter_dim2=0x7f02000c;
-        public static final int meter_dim3=0x7f02000d;
-        public static final int meter_lit=0x7f02000e;
-        public static final int needle=0x7f02000f;
-        public static final int needle_tilted=0x7f020010;
-        public static final int rounded_corners=0x7f020011;
-        public static final int wood_pattern=0x7f020012;
-        public static final int x9l0i24=0x7f020013;
+        public static final int brownbutton=0x7f020000;
+        public static final int dark_bg=0x7f020001;
+        public static final int graybutton=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int logo2=0x7f020005;
+        public static final int meter_dim0=0x7f020006;
+        public static final int meter_dim1=0x7f020007;
+        public static final int meter_dim2=0x7f020008;
+        public static final int meter_dim3=0x7f020009;
+        public static final int meter_lit=0x7f02000a;
+        public static final int needle=0x7f02000b;
+        public static final int needle_tilted=0x7f02000c;
+        public static final int rounded_corners=0x7f02000d;
     }
     public static final class id {
         public static final int button=0x7f070006;
         public static final int dialog_button=0x7f070003;
         public static final int dialog_logo=0x7f070001;
         public static final int dialog_text=0x7f070002;
-        public static final int entry=0x7f07000b;
+        public static final int entry=0x7f070009;
         public static final int imageView1=0x7f070004;
         public static final int instructions=0x7f070007;
-        public static final int label=0x7f07000a;
+        public static final int label=0x7f070008;
         public static final int lin_layout=0x7f070000;
         public static final int meterPanel=0x7f070005;
-        public static final int needle=0x7f070009;
-        public static final int ok=0x7f07000c;
-        public static final int rotator=0x7f070008;
+        public static final int ok=0x7f07000a;
     }
     public static final class layout {
         public static final int dialog=0x7f030000;
         public static final int main=0x7f030001;
-        public static final int meter=0x7f030002;
-        public static final int sliding_main=0x7f030003;
+        public static final int sliding_main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
