0535db6cbf5b4d38c586be08d4c137d8d737317e
octopi <octopie@gmail.com>
2011-12-31 05:09:27 -0500

    version 0.1

--- src/me/davidhu/intensishot/IntensishotActivity.java
+++ src/me/davidhu/intensishot/IntensishotActivity.java
@@ -1,6 +1,10 @@
 package me.davidhu.intensishot;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.drawable.RotateDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -8,12 +12,16 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.text.Html;
 
 public class IntensishotActivity extends Activity implements SensorEventListener {
 
@@ -21,14 +29,7 @@ public class IntensishotActivity extends Activity implements SensorEventListener
 	private Sensor mAccelerometer;
 
 	// UI components
-	private TextView countdownTimer;
-	private TextView xText;
-	private TextView yText;
-	private TextView zText;
 	private Button button;
-	private TextView xDiffText;
-	private TextView yDiffText;
-	private TextView zDiffText;
 	private ImageView meterImage;
 	private RotateDrawable rotator; 
 	private Panel meterPanel;
@@ -38,6 +39,9 @@ public class IntensishotActivity extends Activity implements SensorEventListener
 	
 	// this will continually be replaced as we log more "significant" pounds
 	private CountDownTimer endingTimer;
+	
+	private final int SCORE_DIALOG = 0;
+	private float score;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -46,14 +50,9 @@ public class IntensishotActivity extends Activity implements SensorEventListener
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.main);
 
-		countdownTimer = (TextView) findViewById(R.id.countdownTimer);
-		xText = (TextView) findViewById(R.id.xText);
-		yText = (TextView) findViewById(R.id.yText);
-		zText = (TextView) findViewById(R.id.zText);
 		button = (Button) findViewById(R.id.button);
-		xDiffText = (TextView) findViewById(R.id.xDiffText);
-		yDiffText = (TextView) findViewById(R.id.yDiffText);
-		zDiffText = (TextView) findViewById(R.id.zDiffText);
+		/*TextView instr = (TextView) findViewById(R.id.instructions);
+		instr.setBackgroundResource(R.color.black);*/
 
 		// initialize the sensors
 		mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
@@ -72,25 +71,40 @@ public class IntensishotActivity extends Activity implements SensorEventListener
 			 * After clicking, start countdown. 
 			 */
 			public void onClick(View v) {
-				countdownTimer.setText("Clicked!");
-				new CountDownTimer(6000, 1000) {
+				
+				// reset diffs for each run
+				maxDiff = new float[3];
+				maxDiff[0] = 0;
+				maxDiff[1] = 0;
+				maxDiff[2] = 0;
+				
+				new CountDownTimer(4000, 1000) {
 
 					@Override
 					/**
 					 * After countdown, actually start the sensors. 
 					 */
 					public void onFinish() {
-						countdownTimer.setText("GO!");
+						System.out.println("countdown finished");
+						meterPanel.stopCountdown();
 						startSensing();
 						restartEndTimer();
 					}
 
 					@Override
 					public void onTick(long msLeft) {
-						countdownTimer.setText(String.valueOf(((int)msLeft/1000)));
+						System.out.println(String.valueOf(((int)msLeft/1000)));
+						if(meterPanel.countingDown()) {
+							meterPanel.decreaseCountdown();
+						} else {
+							meterPanel.startCountdown();
+						}
+						meterPanel.invalidate();
 					}
 
 				}.start();
+				
+				button.setClickable(false);
 			}
 		});       
 	}
@@ -104,16 +118,18 @@ public class IntensishotActivity extends Activity implements SensorEventListener
 		mSensorManager.unregisterListener(this);
 		meterPanel.dimMeter();
 		meterPanel.invalidate();
+		zeroes = null; // reset so it'll recalibrate next time
+		button.setClickable(true);
 	}
 
 	protected void onResume() {
 		super.onResume();
-		System.out.println("HERERERERE");
 	}
 
 	protected void onPause() {
 		super.onPause();
 		mSensorManager.unregisterListener(this);
+		stopSensing();
 	}
 
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -122,23 +138,18 @@ public class IntensishotActivity extends Activity implements SensorEventListener
 	public void onSensorChanged(SensorEvent event) {
 		if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
 			float[] values = event.values;
+			
+			// recalibrate on each new run
 			if(zeroes == null) {
 				zeroes = new float[3];
 				zeroes[0] = values[0];
 				zeroes[1] = values[1];
 				zeroes[2] = values[2];
-				maxDiff = new float[3];
-				maxDiff[0] = 0;
-				maxDiff[1] = 0;
-				maxDiff[2] = 0;
 			}
+			
 			float xDiff = Math.abs(values[0]-zeroes[0]);
 			float yDiff = Math.abs(values[1]-zeroes[1]);
 			float zDiff = Math.abs(values[2]-zeroes[2]);
-
-			xText.setText("x: "+String.valueOf(xDiff));
-			yText.setText("y: "+String.valueOf(yDiff));
-			zText.setText("z: "+String.valueOf(zDiff));
 			
 			meterPanel.invalidate(); // tell the panel to draw itself again
 
@@ -156,11 +167,7 @@ public class IntensishotActivity extends Activity implements SensorEventListener
 				maxDiff[2] = zDiff;
 				meterPanel.setAngle(xDiff+yDiff+zDiff);
 				restartEndTimer();
-			}
-
-			xDiffText.setText("x: "+String.valueOf(maxDiff[0]));
-			yDiffText.setText("y: "+String.valueOf(maxDiff[1]));
-			zDiffText.setText("z: "+String.valueOf(maxDiff[2]));    	
+			} 	
 		}
 	}
 	
@@ -174,18 +181,56 @@ public class IntensishotActivity extends Activity implements SensorEventListener
 			
 			@Override
 			public void onTick(long millisUntilFinished) {
-				System.out.println("here");
-				countdownTimer.setText(String.valueOf(((int)millisUntilFinished/1000)));
+				System.out.println(String.valueOf(((int)millisUntilFinished/1000)));
 			}
 			
 			@Override
 			public void onFinish() {
 				stopSensing();
-				float score = maxDiff[0]+maxDiff[1]+maxDiff[2];
-				countdownTimer.setText("Done! Your score is: "+score);
+				
+				// display score
+				score = maxDiff[0]+maxDiff[1]+maxDiff[2];
+				System.out.println("score: "+score);
+				showDialog(SCORE_DIALOG);
+				
 			}
 		}.start();
-		System.out.println("after timer");
+	}
+	
+	protected Dialog onCreateDialog(int id) {
+		final Dialog dialog;
+		switch(id) {
+		case SCORE_DIALOG:
+			dialog = new Dialog(this);
+
+			dialog.setContentView(R.layout.dialog);
+			dialog.setTitle("Your Intensishot Rating: "+Math.floor(score));
+
+			TextView text = (TextView) dialog.findViewById(R.id.dialog_text);
+			if(score < 5) {
+				text.setText(Html.fromHtml("<center><big><b>Meh.</b></big></center><br /><br />Is that seriously the best you can do?"));
+			} else if(score < 10) {
+				text.setText(Html.fromHtml("<center><big><b>Nice!</b></big></center><br /><br />You guys know how to take some shots... now for some more!"));
+			} else if(score < 15) {
+				text.setText(Html.fromHtml("<center><big><b>True champs!</b></big></center><br /><br />Nobody goes as hard as you. Hats off."));
+			} else {
+				text.setText(Html.fromHtml("<center><big><b>Inhuman!</b></big></center><br /><br />Damn. Chill with the roids, man."));
+			}
+			ImageView image = (ImageView) dialog.findViewById(R.id.dialog_logo);
+			image.setImageResource(R.drawable.icon);
+			Button button = (Button) dialog.findViewById(R.id.dialog_button);
+			button.setOnClickListener(new OnClickListener() {
+				
+				public void onClick(View v) {
+					dialog.dismiss();
+					removeDialog(SCORE_DIALOG);
+				}
+			});
+			break;
+		default:
+			dialog = null;	
+		}
+		return dialog;
 	}
 
 }
--- src/me/davidhu/intensishot/Panel.java
+++ src/me/davidhu/intensishot/Panel.java
@@ -6,7 +6,9 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 
 class Panel extends View {
 
@@ -18,9 +20,13 @@ class Panel extends View {
 	private Matrix rotationMatrix;
 	
 	private Bitmap needleBitmap;
-	private Bitmap meterDim;
+	private Bitmap[] meters;
 	private Bitmap meterLit;
 	private Bitmap currMeter;
+	private Paint textPaint;
+	private int textSize;
+	private boolean countdown;
+	private int currCountdown;
 
 
 	public Panel(Context context, AttributeSet attrs)
@@ -29,12 +35,26 @@ class Panel extends View {
 		angle = ZERO_METER;
 		
 		needleBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.needle_tilted);
-		meterDim = BitmapFactory.decodeResource(getResources(), R.drawable.meter_dim);
+		meters = new Bitmap[4];
+		meters[0] = BitmapFactory.decodeResource(getResources(), R.drawable.meter_dim0);
+		meters[1] = BitmapFactory.decodeResource(getResources(), R.drawable.meter_dim1);
+		meters[2] = BitmapFactory.decodeResource(getResources(), R.drawable.meter_dim2);
+		meters[3] = BitmapFactory.decodeResource(getResources(), R.drawable.meter_dim3);
 		meterLit = BitmapFactory.decodeResource(getResources(), R.drawable.meter_lit);
-		currMeter = meterDim;
+		
+		textPaint = new Paint();
+		textSize = 64;
+		textPaint.setColor(Color.WHITE);
+		textPaint.setTextSize(72);
+		textPaint.setAntiAlias(true);
+		countdown = false;
+		currCountdown = 3;
+		
+		currMeter = meters[0];
 	}
 	
 	public void onDraw(Canvas canvas) {
+		System.out.println(canvas.getDensity());
 		rotationMatrix = new Matrix();
 		
 		if(angle > ZERO_METER) {
@@ -48,18 +68,23 @@ class Panel extends View {
 			angle = ZERO_METER;
 		}
 		
-		rotationMatrix.postRotate(angle, 185, 97); // rotating to given angle
-		rotationMatrix.postTranslate(30, 114); // constant offset
+		//rotationMatrix.postRotate(angle, 185, 97); // rotating to given angle
+		System.out.println(canvas.getWidth()+"x"+canvas.getHeight());
+		/*rotationMatrix.postRotate(angle, 170*(480/canvas.getWidth()), 90*(480/canvas.getWidth())); // rotating to given angle
+		rotationMatrix.postTranslate(40*(480/(canvas.getWidth()+100)), 118*(800/(canvas.getHeight()+400))); // constant offset*/
+		rotationMatrix.postRotate(angle, 110, 54); // rotating to given angle
+		rotationMatrix.postTranslate(36, 84); // constant offset
 
 		canvas.drawBitmap(needleBitmap, rotationMatrix, null);
 
 		canvas.drawBitmap(currMeter, 0, 0, null);
+		
 	}
 	
 	
 	public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		setMeasuredDimension(431, 220); // dimensions of meter
+		//setMeasuredDimension(431, 220); // dimensions of meter
 	}
 
 	// sets the angle given some intensity
@@ -70,11 +95,30 @@ class Panel extends View {
 		System.out.println("intensity is "+intensity+" angle is "+angle);
 	}
 	
+	public void startCountdown() {
+		countdown = true;
+		currCountdown = 1;
+		currMeter = meters[1];
+	}
+	public void stopCountdown() {
+		countdown = false;
+	}
+	public void decreaseCountdown() {
+		if(currCountdown < 3) {
+			currCountdown++;
+			currMeter = meters[currCountdown];
+		}
+	}
+	public boolean countingDown() {
+		return countdown;
+	}
+	
+	
 	public void lightMeter() {
 		currMeter = meterLit;
 	}
 	public void dimMeter() {
-		currMeter = meterDim;
+		currMeter = meters[0];
 	}
 
 }
